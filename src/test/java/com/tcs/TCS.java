350a27d854e3ce4c7c3dda8b9ca317ec53ceca35
dineshraja45 <dineshraja120496@gmail.com>
2022-12-22 16:44:34 +0530

    ADD

---
+++ src/test/java/com/tcs/TCS.java
@@ -0,0 +1,16 @@
+package com.tcs;
+
+public class TCS {
+
+	
+	
+	
+	private void m1() {
+		// TODO Auto-generated method stub
+
+	}
+	private void m2() {
+		// TODO Auto-generated method stub
+
+	}
+}
